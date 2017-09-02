a3326b856b3001230b6fe035785813a01cc3b069
lsq <liushangqing9803@163.com>
2017-09-02 18:17:10 +0800

    SQL建表语句

--- src/main/resources/db_blog.sql
+++ src/main/resources/db_blog.sql
@@ -1,40 +1,108 @@
-DROP DATABASE IF EXISTS db_blog;
-/*创建数据库，并设置编码*/
-CREATE DATABASE db_blog DEFAULT CHARACTER SET utf8;
+/*
+Navicat MySQL Data Transfer
 
-USE db_blog;
+Source Server         : blog
+Source Server Version : 50528
+Source Host           : localhost:3306
+Source Database       : db_blog
 
-CREATE TABLE `t_blogger` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '博主id',
-  `username` VARCHAR(50) NOT NULL COMMENT '博主姓名',
-  `password` VARCHAR(100) NOT NULL COMMENT '博主密码',
-  `profile` TEXT COMMENT '博主信息',
-  `nickname` VARCHAR(50) DEFAULT NULL COMMENT '博主昵称',
-  `sign` VARCHAR(100) DEFAULT NULL COMMENT '博主签名',
-  `imagename` VARCHAR(100) DEFAULT NULL COMMENT '博主头像路径',
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+Target Server Type    : MYSQL
+Target Server Version : 50528
+File Encoding         : 65001
 
+Date: 2017-09-02 18:08:55
+*/
 
-CREATE TABLE `t_blogtype` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '博客id',
-  `typeName` VARCHAR(30) DEFAULT NULL COMMENT '博客类别',
-  `orderNum` INT(11) DEFAULT NULL COMMENT '博客排序',
-  PRIMARY KEY (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+SET FOREIGN_KEY_CHECKS=0;
 
-
+-- ----------------------------
+-- Table structure for t_blog
+-- ----------------------------
+DROP TABLE IF EXISTS `t_blog`;
 CREATE TABLE `t_blog` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '博客类型',
-  `title` VARCHAR(200) NOT NULL COMMENT '博客题目',
-  `summary` VARCHAR(400) DEFAULT NULL COMMENT '博客摘要',
-  `releaseDate` DATETIME DEFAULT NULL COMMENT '发布日期',
-  `clickHit` INT(11) DEFAULT NULL COMMENT '评论次数',
-  `replyHit` INT(11) DEFAULT NULL COMMENT '回复次数',
-  `content` TEXT COMMENT '博客内容',
-  `keyWord` VARCHAR(200) DEFAULT NULL COMMENT '关键字',
-  `type_id` INT(11) DEFAULT NULL COMMENT '外键关联博客类别',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '博客类型',
+  `title` varchar(200) NOT NULL COMMENT '博客题目',
+  `summary` varchar(400) DEFAULT NULL COMMENT '博客摘要',
+  `releaseDate` datetime DEFAULT NULL COMMENT '发布日期',
+  `clickHit` int(11) DEFAULT NULL COMMENT '评论次数',
+  `replyHit` int(11) DEFAULT NULL COMMENT '回复次数',
+  `content` text COMMENT '博客内容',
+  `keyWord` varchar(200) DEFAULT NULL COMMENT '关键字',
+  `type_id` int(11) DEFAULT NULL COMMENT '外键关联博客类别',
   PRIMARY KEY (`id`),
   KEY `type_id` (`type_id`),
   CONSTRAINT `t_blog_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `t_blogtype` (`id`)
-) ENGINE=INNODB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_blog
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_blogger
+-- ----------------------------
+DROP TABLE IF EXISTS `t_blogger`;
+CREATE TABLE `t_blogger` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '博主id',
+  `username` varchar(50) NOT NULL COMMENT '博主姓名',
+  `password` varchar(100) NOT NULL COMMENT '博主密码',
+  `profile` text COMMENT '博主信息',
+  `nickname` varchar(50) DEFAULT NULL COMMENT '博主昵称',
+  `sign` varchar(100) DEFAULT NULL COMMENT '博主签名',
+  `imagename` varchar(100) DEFAULT NULL COMMENT '博主头像路径',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_blogger
+-- ----------------------------
+INSERT INTO `t_blogger` VALUES ('2', 'admin', 'c4ca4238a0b923820dcc509a6f75849b', null, null, null, null);
+
+-- ----------------------------
+-- Table structure for t_blogtype
+-- ----------------------------
+DROP TABLE IF EXISTS `t_blogtype`;
+CREATE TABLE `t_blogtype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '博客id',
+  `type_name` varchar(30) DEFAULT NULL COMMENT '博客类别',
+  `order_num` int(11) DEFAULT NULL COMMENT '博客排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_blogtype
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `t_comment`;
+CREATE TABLE `t_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `state` varchar(255) DEFAULT NULL,
+  `user_ip` varchar(255) DEFAULT NULL,
+  `content` text,
+  `blog_id` int(11) DEFAULT NULL,
+  `comment_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_link
+-- ----------------------------
+DROP TABLE IF EXISTS `t_link`;
+CREATE TABLE `t_link` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `link_name` varchar(255) DEFAULT NULL,
+  `link_url` varchar(255) DEFAULT NULL,
+  `order_num` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_link
+-- ----------------------------
